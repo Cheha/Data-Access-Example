58b271d78520b10fd04d59bf7ecdda16cd7a3867
Alex <steepsanya@bigmir.net>
2016-07-22 02:40:43 +0300

    Update Delete Example

---
+++ Example_26_DataAdapter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Example_26_DataAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Nordwind"].ConnectionString;
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Categories", connectionString);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+
+            foreach (DataRow row in dataSet.Tables[0].Rows) {
+                int categoryId = (int)row[0];
+                string categoryName = row[1].ToString();
+                string description = row[2].ToString();
+                Console.WriteLine("{0}, {1}, {2}", categoryId, categoryName, description);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example_27_UpdateDataUsingDataAdapter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_27_UpdateDataUsingDataAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Nordwind"].ConnectionString;
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Categories", connectionString);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+
+            //foreach (DataRow row in dataSet.Tables[0].Rows)
+            //{
+            //    // Delete Row
+            //    if ((int)row[0] == 10) {
+            //        dataSet.Tables[0].Rows.Remove(row);
+            //        dataSet.Tables[0].AcceptChanges();
+            //        adapter.Update(dataSet);
+            //        break;
+            //    }
+            //}
+
+            
+            // Insert row
+            DataRow newCategory = dataSet.Tables[0].NewRow();
+            newCategory["CategoryName"] = "Suplies";
+            newCategory["Description"] = "Some stuff";
+            dataSet.Tables[0].Rows.Add(newCategory);
+            dataSet.Tables[0].AcceptChanges();
+            adapter.Update(dataSet);
+            
+            Console.WriteLine("Categories sucessfully updated");
+            Console.ReadLine();
+        }
+    }
+}
