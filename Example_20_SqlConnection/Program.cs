bd85f7f2c6eba1a92f3144fad777acadf2d721e2
Alex <steepsanya@bigmir.net>
2016-07-22 01:27:27 +0300

    SqlConnection Example

---
+++ Example_20_SqlConnection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_20_SqlConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data source = .; Initial Catalog = NORTHWND; Integrated Security = true";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();            
+        }
+    }
+}
