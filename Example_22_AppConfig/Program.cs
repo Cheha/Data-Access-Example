861ea5b81209ef9cfcd4621060c6e39654628b50
Alex <steepsanya@bigmir.net>
2016-07-22 02:04:49 +0300

    App.config example

---
+++ Example_22_AppConfig/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_22_AppConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Nordwind"].ConnectionString;
+            string query = "SELECT * FROM Categories";
+            SqlConnection connection = new SqlConnection(connectionString);
+            using (SqlCommand command = new SqlCommand(query, connection)) {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read())
+                    {
+                        int categoryId = (int)reader[0];
+                        string categoryName = reader[1].ToString();
+                        string description = reader[2].ToString();
+                        Console.WriteLine("{0}, {1}, {2}", categoryId, categoryName, description);
+                    }
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
