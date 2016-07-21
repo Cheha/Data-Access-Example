63a342376cfc097e89a96a19892b07ed13bbfc27
Alex <steepsanya@bigmir.net>
2016-07-22 02:12:09 +0300

    ExecuteNonQuery Example

---
+++ Example_23_ExecuteNonQuery/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Example_23_ExecuteNonQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Nordwind"].ConnectionString;
+            string query = "INSERT INTO Categories (CategoryName, Description ) VALUES ('Chemistry', 'Soap')";
+            SqlConnection connection = new SqlConnection(connectionString);
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.ExecuteNonQuery();   
+            }
+            Console.WriteLine("Insert was successfull");
+            Console.ReadLine();
+        }
+    }
+}
