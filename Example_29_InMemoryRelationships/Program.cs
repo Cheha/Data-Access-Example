05d688307a09b1158ae2072546dd8719a07f6e08
Alex <steepsanya@bigmir.net>
2016-07-24 21:17:23 +0300

    Relationships

---
+++ Example_29_InMemoryRelationships/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Example_29_InMemoryRelationships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Nordwind"].ConnectionString;
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Categories; SELECT * FROM Products;", connectionString);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            // dataSet.Tables
+            DataTable categoriesTable = dataSet.Tables[0];
+            DataTable productsTable = dataSet.Tables[1];
+
+            DataRelation relation = new DataRelation("ProductCategories",
+                categoriesTable.Columns["CategoryID"],
+                productsTable.Columns["CategoryID"]);
+            dataSet.Relations.Add(relation);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example_30_ConnectedModeTransaction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_30_ConnectedModeTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Nordwind"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString)) { 
+                
+            }
+        }
+    }
+}
