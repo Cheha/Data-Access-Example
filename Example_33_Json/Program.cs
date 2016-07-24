acc30e507db622b930906c16e94c6965a097c8a7
Alex <steepsanya@bigmir.net>
2016-07-24 23:22:58 +0300

    Json Example

---
+++ Example_33_Json/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_33_Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { Name = "Alex", Age = 44 };
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Person));
+            MemoryStream stream = new MemoryStream();
+            serializer.WriteObject(stream, person);
+            string jsonString = Encoding.UTF8.GetString(stream.ToArray());
+            stream.Close();
+            Console.WriteLine(jsonString);
+
+            // Deserializing
+            MemoryStream newStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+            Person newPerson = (Person)serializer.ReadObject(newStream);
+            Console.WriteLine("{0} {1}", newPerson.Age, newPerson.Name);
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
