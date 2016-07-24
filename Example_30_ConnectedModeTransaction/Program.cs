d60815b91e41799e42b40632e9ddbdfba42a3fb5
Alex <steepsanya@bigmir.net>
2016-07-24 21:25:31 +0300

    Transaction: Connected mode

--- Example_30_ConnectedModeTransaction/Program.cs
+++ Example_30_ConnectedModeTransaction/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Configuration;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -15,8 +14,32 @@ namespace Example_30_ConnectedModeTransaction
         {
             string connectionString = ConfigurationManager.ConnectionStrings["Nordwind"].ConnectionString;
 
-            using (SqlConnection connection = new SqlConnection(connectionString)) { 
-                
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+                SqlCommand command = connection.CreateCommand();
+                command.Transaction = transaction;
+
+                try
+                {
+                    command.CommandText = @"INSERT INTO Region VALUES(5, 'Central')";
+                    command.ExecuteNonQuery();
+                    command.CommandText = @"INSERT INTO Region VALUES(6, 'Central')";
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                    Console.WriteLine("OK");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception rollbackEx) {
+                        Console.WriteLine(rollbackEx.Message);
+                    }
+                }
             }
         }
     }
