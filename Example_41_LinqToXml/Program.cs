e77650075997c8b0a2312d1967fdb02799bd0c8e
Alex <steepsanya@bigmir.net>
2016-07-26 23:24:33 +0300

    Linq To Xml

---
+++ Example_41_LinqToXml/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Example_41_LinqToXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement root = XElement.Load("LINQOperationXML.xml");
+
+            var contacts = from el in root.Elements("contact")
+                           where el.Element("lastName").Value == "Springwater"
+                           select el;
+            foreach(var el in contacts) {
+                Console.WriteLine(el);
+            }
+            Console.ReadLine();
+        }
+    }
+}
