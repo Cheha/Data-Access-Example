ac82be9690d3929eecb57f57998b21e63d70f8f2
Alex <steepsanya@bigmir.net>
2016-07-26 22:58:08 +0300

    Extension method

---
+++ Example_38_Extention_method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_38_Extension_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is custom string for extension method";
+            int i = str.WordCount();
+            Console.WriteLine("Word count: {0}", i);
+            Console.ReadLine();
+        }
+    }
+
+    public static class Extension
+    { 
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] {' ', '.', ',', '?', '!'}, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+
+}
